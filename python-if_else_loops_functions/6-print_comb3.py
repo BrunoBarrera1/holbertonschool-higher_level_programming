890c8c01cf0d8f0d9ebff6b32f6190dc614feaaa
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-09 15:14:28 -0300

    a

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -7,5 +7,3 @@ for i in range(10):
                 print("{}{}".format(i, j), end="\n")
             else:
                 print("{}{}".format(i, j), end=", ")
-                
-                #BASTA
