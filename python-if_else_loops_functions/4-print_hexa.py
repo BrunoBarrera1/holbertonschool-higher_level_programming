ec1ae08d6bf044fced396e59a40a880c33b075cf
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-09 11:33:23 -0300

    a

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+for gaston in range(0, 98):
+    print(f"{gaston} = 0x{gaston}")
---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+for colorado in range(0, 99):
+    print(f"{colorado},", end="")
