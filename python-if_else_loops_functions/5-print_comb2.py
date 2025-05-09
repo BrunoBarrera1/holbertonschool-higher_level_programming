0972808eacc95f4b66d7a4312e435c9cf9e331ab
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-09 12:08:03 -0300

    aa

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for cape in range(0, 100):
+for cape in range(0, 99):
     print(f"{cape}, ", end="")
-    if cape == 99:
-        print("99 ")
+    if cape == 98:
+        print("99")
     
