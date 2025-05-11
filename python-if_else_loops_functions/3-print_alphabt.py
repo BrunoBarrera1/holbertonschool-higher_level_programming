a9c8890b1db942022e5896062a5a63a3b9439038
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-11 13:04:58 -0300

    a

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@ no = "e"
 no1 = "q"
 for juan in range (97, 123):
     character = chr(juan)
-    if juan != no and juan != no1:
-        print(juan, end="".format(juan))
+    if character != no and character != no1:
+        print(character, end="".format(character))
