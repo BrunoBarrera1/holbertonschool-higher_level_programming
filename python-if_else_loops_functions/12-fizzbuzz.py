153c4d3356d7a24715d1ebbd6336717f08e1764a
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-11 12:45:00 -0300

    a

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,8 @@
+def fizzbuzz():
+    number = 1
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz ")
+    elif number % 3 == 0:
+        print("Fizz ")
+    elif number % 5 == 0:
+        print("Buzz ")
