63c7d51736a79de6bd389c7705940c33333bde19
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-13 10:37:25 -0300

    a

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        copied_list = my_list.copy()
-        return copied_list
-
-    elif idx >= len(my_list):
-        return my_list
+    bruno = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return bruno
+    bruno[idx] = element
+    return bruno
