5b5141bb1a7e9fcf2bcbdcc402e6eaee9e64d341
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-21 09:57:40 -0300

    a

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,15 +4,16 @@ Module: 1-square
 Defines class square with a private sie attribute.
 """
 
+
 class Square:
     """
     Class Square that defines a square via a private instance attribute: size
 
     private instance attribute:
-    __size (int): the size of the square, set on instantiation without validation.
+    __size (int): the size of square set instantiation without validation.
 
     Why private?
-    the size of a square is funcamental for operations like area computation. by making it private,
+    the size of square is fundamental for operations by making it private,
     the class builder can control andvalidate type and value in future tasks.
     """
     def __init__(self, size):
