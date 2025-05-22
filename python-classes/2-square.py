859b1ec53ff2ab7678651a2b31b375af17a2195b
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-22 11:56:01 -0300

    a

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -16,7 +16,7 @@ class Square:
         typeerroR: if size is not an integer
         valueerror: if size is less than 0
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
