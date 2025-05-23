da3bb901989f323eaddedafd7fdb67e94cf8d992
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-23 16:04:09 -0300

    a

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """represents square"""
+
+    def __init__(self, size=0):
+        """initializes square"""
+        self.size = size
+
+        @property
+        def size(self):
+            """returns size of square"""
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            """establishes value of sq"""
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >=0")
+            self.__size = value
+
+            def area(self):
+                """prints the square with character #"""
+                if self.__size == 0:
+                    print()
+                else:
+                    for _ in range(self.__size):
+                        print("#" * self.__size)
