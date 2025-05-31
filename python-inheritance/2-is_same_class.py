3a07dd3b6c015f1781d8845f0631503f6b73d774
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-30 23:00:04 -0300

    a

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module provides function to check if an object is
+exactly an instane of a class excluding inheritance.
+"""
+
+
+def is_same_class(obj, a_class)
+    """
+    im tired
+    """
+    return type(obj) is a_class
