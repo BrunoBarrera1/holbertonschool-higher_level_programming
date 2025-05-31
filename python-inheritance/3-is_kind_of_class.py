3aa2b4d535142682245e676f90f9517be8f14b8b
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-05-31 19:45:33 -0300

    a

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module providing function to check wether object is instance
+of class or inherits from it
+"""
+
+
+def is_kind_of_clas(obj, a_class):
+    """
+    a
+    """
+    return isinstance(obj, a_class)
