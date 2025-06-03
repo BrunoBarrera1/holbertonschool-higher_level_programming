e7741fed8c79176edbd687f1085faf795b1d8910
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-06-03 09:18:16 -0300

    a

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+this module defines a function that prints a file
+"""
+
+
 def read_file(filename=""):
+    """function that does so"""
     with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
