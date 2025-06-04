b6df0c634b640d86fe9f10efa1e3d2fca76c575b
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-06-04 13:06:13 -0300

    a

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that appends string to end of text"""
+
+
+def append_write(filename="", text=""):
+    with open(filename="", "w" text="") as file:
+        file.write(text="" + '\n')
