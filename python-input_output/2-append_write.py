44039efa479a4f0375c2e70caad511edbdf5c929
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-06-04 13:16:42 -0300

    a

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 
 def append_write(filename="", text=""):
     """appends"""
-    with open(filename=, "a", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
