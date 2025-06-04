02cf8bfcd3af2d0ae128d73c9c4042713dc8f787
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-06-04 11:33:28 -0300

    a

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,5 @@ this block contains a function
 def write_file(filename="", text=""):
     """function that writes the file"""
     text = "hello"
-    with open(filename, "w", encoding="utf-8") as f:
-        result = f.write(text)
-        return(result)
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
