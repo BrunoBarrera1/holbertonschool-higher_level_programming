4a33a85a4acad862e5d2853bdcaac6a6bae8ece9
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-06-08 20:27:00 -0300

    a

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
-"""module that is a module"""
+"""
+This module provides functionality for JSON serialization.
+It contains a function that converts a Python object to its
+JSON string representation.
+"""
+import json
 
 
 def to_json_string(my_obj):
-    """returns json rep of an obj"""
+    """
+    Returns the JSON string representation of an object.
+
+    This function takes a Python object and converts it to
+    a JSON formatted string using the json module.
+
+    Args:
+        my_obj: The Python object to be converted to JSON string
+
+    Returns:
+        str: A JSON formatted string representation of the object
+
+    Note:
+        - Does not handle exceptions if the object can't be serialized
+        - Common serializable types include:
+            dict, list, str, int, float, bool, None
+        - Non-serializable types (like sets) will raise TypeError
+    """
     return json.dumps(my_obj)
