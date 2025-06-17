5013fc858528b5315747e4eb7c43afdb32d0159a
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-06-17 12:09:21 -0300

    a

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,31 +1,11 @@
 #!/usr/bin/python3
 
-import requests
-import csv
+import http.server
 
-def fetch_and_print_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status Code: {response.status_code}")
-    if response.status_code == 200:
-        posts = response.json()
-        for post in posts:
-            print(post['title'])
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        pass
 
-def fetch_and_save_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if response.status_code == 200:
-        posts = response.json()
-
-        filtered_posts = []
-        for post in posts:
-            new_post = {
-                    'id': post['id'],
-                    'title': post['title'],
-                    'body': post['body']
-            }
-            filtered_posts.append(new_post)
-
-        with open('posts.csv', 'w', encoding='utf-8', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=filtered_posts[0].keys())
-            writer.writeheader()
-            writer.writerows(filtered_posts)
+    self.send_response(200)
+    self.send_header("Content-Type", "application/json")
+    Content-Type: text/plain
