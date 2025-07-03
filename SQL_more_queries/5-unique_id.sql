609220bdf461b43eb56778da3514ea5b1a33a352
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-07-03 09:55:38 -0300

    a

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
--- Script to create the table 'unique_id' with a unique 'id' and default value 1
-=======
--- Script que crea la tabla unique_id donde 'id' es UNIQUE y tiene valor por defecto 1
->>>>>>> f0382271a53fcd72acbab27632922f745edc7f2a
+-- Create table unique_id with id INT DEFAULT 1 and UNIQUE constraint on id
 
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
+    id INT NOT NULL DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
 
--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
--- Script to create the database hbtn_0d_usa and table states with constraints
-=======
--- Script para crear la base de datos hbtn_0d_usa y la tabla states
->>>>>>> f0382271a53fcd72acbab27632922f745edc7f2a
-
+-- Create database hbtn_0d_usa if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the database
 USE hbtn_0d_usa;
 
+-- Create table states if not exists
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL UNIQUE
 );
 
